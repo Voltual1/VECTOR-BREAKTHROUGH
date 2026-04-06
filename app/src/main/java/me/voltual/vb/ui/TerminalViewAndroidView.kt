3dd7b8d6c313a8c65f5bcebd8b8bedf750e195fc
Voltual1 <18206039173@163.com>
2026-04-06 10:34:11 +0800

    update

--- app/src/main/java/me/voltual/vb/ui/TerminalViewAndroidView.kt
+++ app/src/main/java/me/voltual/vb/ui/TerminalViewAndroidView.kt
@@ -20,7 +20,7 @@ fun TerminalViewAndroidView(
         factory = { ctx ->
             TerminalView(ctx, null).apply {
                 // 必须设置文本大小，否则 mRenderer 为 null 导致崩溃
-                setTextSize(16)
+                setTextSize(128)
 
                 setTerminalViewClient(object : TerminalViewClient {
                     override fun onScale(scale: Float) = 1f
